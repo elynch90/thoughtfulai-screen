ec88f099f1430a793e47d4ff9a81b96b122ba208
Elliott <48255160+elynch90@users.noreply.github.com>
2024-09-07 10:23:40 -0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -14,7 +14,7 @@ def sort(width: float, height: float, length: float, mass: float) -> str:
     Returns:
     str
         The label of the package for sorting."""
-    assert any([i > 0 for i in [width, height, length]]
+    assert any([i > 0 for i in [width, height, length, mass]]
                ), "cannot compute on dimensions with negative or zero values."
     # avoid magic numbers by defining constants
     bulk_max = 1_000_000
